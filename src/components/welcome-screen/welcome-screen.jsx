c0255d88c37c202e216a6ae0762a273c421452ac
Dmitry Orlenko <orlenko.dmitry.dev@gmail.com>
2019-10-18 11:18:59 +0300

    add propTypes validation for WelcomeScreen

--- src/components/welcome-screen/welcome-screen.jsx
+++ src/components/welcome-screen/welcome-screen.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import {number} from 'prop-types';
 
-const WelcomeScreen = () => (
+const WelcomeScreen = ({minutes, mistakesNumber}) => (
   <section className="welcome">
     <div className="welcome__logo">
       <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83" />
@@ -11,11 +12,16 @@ const WelcomeScreen = () => (
     <h2 className="welcome__rules-title">Правила игры</h2>
     <p className="welcome__text">Правила просты:</p>
     <ul className="welcome__rules-list">
-      <li>За 5 минут нужно ответить на все вопросы.</li>
-      <li>Можно допустить 3 ошибки.</li>
+      <li>За {minutes} минут нужно ответить на все вопросы.</li>
+      <li>Можно допустить {mistakesNumber} ошибки.</li>
     </ul>
     <p className="welcome__text">Удачи!</p>
   </section>
 );
+
+WelcomeScreen.propTypes = {
+  minutes: number.isRequired,
+  mistakesNumber: number.isRequired,
+};
 
 export default WelcomeScreen;
