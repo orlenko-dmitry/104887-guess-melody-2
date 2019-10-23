266d2898e86fe3f082fe16ace0dd5dca07e89e86
Dmitry Orlenko <orlenko.dmitry.dev@gmail.com>
2019-10-23 10:24:33 +0300

    add GameArtist

--- src/components/game-artist/game-artist.jsx
+++ src/components/game-artist/game-artist.jsx
@@ -1,7 +1,62 @@
 import React from 'react';
+import {arrayOf, shape, string} from 'prop-types';
 
-const GameArtist = () => {
+const GameArtist = ({answers}) => (
+  <section className="game game--artist">
+    <header className="game__header">
+      <a className="game__back" href="#">
+        <span className="visually-hidden">Сыграть ещё раз</span>
+        <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
+      </a>
 
+      <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
+        <circle className="timer__line" cx="390" cy="390" r="370" style="filter: url(#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center" />
+      </svg>
+
+      <div className="timer__value" xmlns="http://www.w3.org/1999/xhtml">
+        <span className="timer__mins">05</span>
+        <span className="timer__dots">:</span>
+        <span className="timer__secs">00</span>
+      </div>
+
+      <div className="game__mistakes">
+        <div className="wrong"></div>
+        <div className="wrong"></div>
+        <div className="wrong"></div>
+      </div>
+    </header>
+
+    <section className="game__screen">
+      <h2 className="game__title">Кто исполняет эту песню?</h2>
+      <div className="game__track">
+        <div className="track">
+          <button className="track__button track__button--play" type="button"></button>
+          <div className="track__status">
+            <audio></audio>
+          </div>
+        </div>
+      </div>
+
+      <form className="game__artist">
+        {answers.map(({picture, artist}, index) => (
+          <div className="artist" key={`${artist}-${index}`}>
+            <input className="artist__input visually-hidden" type="radio" name="answer" value="artist-1" id="answer-1" />
+            <label className="artist__name" htmlFor="answer-1">
+              <img className="artist__picture" src={picture} alt="Пелагея" />
+              {artist}
+            </label>
+          </div>
+        ))}
+      </form>
+    </section>
+  </section>
+);
+
+GameArtist.propTypes = {
+  answers: arrayOf(shape({
+    picture: string,
+    artist: string,
+  })),
 };
 
 export default GameArtist;
