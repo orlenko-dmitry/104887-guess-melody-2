e0eb0cb356b923b398bb88b1a5ce3c46466289f7
Dmitry Orlenko <orlenko.dmitry.dev@gmail.com>
2019-10-28 09:04:14 +0200

    refactor game-genre.e2e.test

--- src/components/game-genre/game-genre.e2e.test.js
+++ src/components/game-genre/game-genre.e2e.test.js
@@ -4,22 +4,29 @@ import {mount} from 'enzyme';
 import GameGenre from './game-genre.jsx';
 import questions from '../../mocks/questions.js';
 
-const onSetAnswerClick = jest.fn();
-const wrapper = mount(
-    <GameGenre
-      gameData={questions[1]}
-      onSetAnswerClick={onSetAnswerClick}
-    />
-);
-const submitBtn = wrapper.find(`.game__submit`);
-
-submitBtn.simulate(`click`);
-
 describe(`Test for GameGenre`, () => {
   it(`onSetAnswerClick have been called 1 time`, () => {
+    const onSetAnswerClick = jest.fn();
+    const wrapper = mount(
+        <GameGenre
+          gameData={questions[1]}
+          onSetAnswerClick={onSetAnswerClick}
+        />
+    );
+    const submitBtn = wrapper.find(`.game__submit`);
+    submitBtn.simulate(`click`, {preventDefault() {}});
     expect(onSetAnswerClick).toHaveBeenCalledTimes(1);
   });
   it(`onSetAnswerClick calls its callback with an array`, () => {
+    const onSetAnswerClick = jest.fn();
+    const wrapper = mount(
+        <GameGenre
+          gameData={questions[1]}
+          onSetAnswerClick={onSetAnswerClick}
+        />
+    );
+    const submitBtn = wrapper.find(`.game__submit`);
+    submitBtn.simulate(`click`, {preventDefault() {}});
     expect(onSetAnswerClick).toBecalledWith(expect.any(Array));
   });
 });
